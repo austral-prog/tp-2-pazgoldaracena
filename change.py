5b5efbd46b3c7f9e18eab53e4fc77f137629101b
pazgoldaracena <pgoldaracena@mail.austral.edu.ar>
2025-03-28 13:14:17 -0300

    Update change.py

--- change.py
+++ change.py
@@ -5,14 +5,14 @@ def change():
     print(expense)
     print("Dinero recibido")
     print(money)
+    print()
     print("Vuelto")
+    print()
     print("Pesos:")
     vuelto = money - expense
     pesos = int(vuelto)
     print(pesos)
-    print("Decimal:")
+    print("Centavos:")
     decimal = vuelto - pesos
     decimal_entero = int(decimal * 100)
     print(decimal_entero)
-
-change()
